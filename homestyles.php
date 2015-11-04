c64619519306674137e9941b340be949a972b107
Quinten Lambert <quinten_lambert@berea.edu>
2015-11-04 11:04:45 -0500

    Started adding the table to the style page and making sure that the functionality remains.

--- homestyles.php
+++ homestyles.php
@@ -10,7 +10,7 @@
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
 
-    <title>Narrow Jumbotron Template for Bootstrap</title>
+    <title>Faculty Tracking</title>
 
     <!-- Bootstrap core CSS -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -42,36 +42,67 @@
         </nav>
         <h3 class="text-muted">Project name</h3>
       </div>
-
+<CENTER>
       <div class="jumbotron">
-        <h1>Jumbotron heading</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <h1>Home Directory</h1>
+        <p class="lead">
+		<table border=2><tbody>
+		<tr><th> <h3><CENTER>Faculty</CENTER></h3> </th><th> <h3><CENTER>Activity</CENTER></h3> </th></tr>
+		<!--begin row 1-->
+		<tr><th><form action = "singlesearch.php" method = "post">
+				 <input type="Submit" value = "Faculty Single Search" name = "PageChange"/>
+				 </form>
+		</th><th><form action = "singlesearch.php" method = "post">
+				 <input type="Submit" value = "Activity Single Search" name = "PageChange"/>
+				 </form>
+		</th></tr>
+		<!--end row 1-->
+		<!--begin row 2-->
+		<tr><th><form action = "singlesearch.php" method = "post">
+				<input type="Submit" value = "View All Faculty" name = "PageChange"/>
+				</form>
+		</th><th><form action = "singlesearch.php" method = "post">
+				<input type="Submit" value = "View All Activities" name = "PageChange"/>
+				</form>
+		</th></tr>
+		<!--end row 2-->
+		<!--begin row 3-->
+		<tr><th><form action = "singlesearch.php" method = "post">
+				<input type="Submit" value = "Add Faculty" name = "PageChange"/> 
+				</form>
+		</th><th><form action = "singlesearch.php" method = "post">
+				<input type="Submit" value = "Add Activities" name = "PageChange"/>
+				</form>
+		</th></tr>
+		<!--end row 3-->
+		<!--begin row 4-->
+		<tr><th><form action = "singlesearch.php" method = "post">
+				<input type="Submit" value = "Edit Faculty" name = "PageChange"/> 
+				</form>
+		</th><th><form action = "singlesearch.php" method = "post">
+				<input type="Submit" value = "Edit Activities" name = "PageChange"/>
+				</form>
+		</th></tr>
+		<!--end row 4-->
+		<!--begin row 5-->
+		<tr><th><form action = "singlesearch.php" method = "post">
+				<input type="Submit" value = "Remove Faculty" name = "PageChange"/> 
+				</form>
+		</th><th><form action = "singlesearch.php" method = "post">
+				<input type="Submit" value = "Remove Activities" name = "PageChange"/>
+				</form>
+		</th></tr>
+		<!--end row 5-->
+
+		</tbody></table>
+		<form action = "http://tango.berea.edu/lambertq/FacultyDB/advancedsearch/advsearch.php" method = "post">
+				<input type="Submit" value = "Advanced Search" name = "PageChange"/>
+				</form>
+	
+		</p>
         <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
       </div>
-
-      <div class="row marketing">
-        <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
-
-        <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
-      </div>
+</CENTER>
 
       <footer class="footer">
         <p>&copy; Company 2014</p>
